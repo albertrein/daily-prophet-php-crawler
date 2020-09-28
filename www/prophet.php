3667b43085a54489327d48ca7caf191f041224c7
albertrein <guilhermealberto8@gmail.com>
2020-09-28 09:00:49 -0300

    Getting countries autoload, to capture array os objects.

--- www/prophet.php
+++ www/prophet.php
@@ -1,9 +1,10 @@
 <?php
-print_r($_COOKIE);
+//print_r($_COOKIE);
+
+require('countries/autoload.php');
 require('vendor/autoload.php');
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\CssSelector\CssSelectorConverter;
-
 
 $html = file_get_contents("https://oglobo.globo.com/");
 //$html = "<p>teste1gg</p>";
@@ -16,39 +17,3 @@ $paragraph = $crawler->filter('.teaser__subtitle.headline__subtitle')->each(func
     return $node->text();
 });
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Testing muggles-news font</title>
-    <style>
-    	body{background-color: #ebd4b9}
-        @font-face {
-            font-family: 'mugglenews';
-            src: URL('./mugglenews_by_nathanthenerd_d41qn8p.ttf') format('truetype');
-        }
-        h1{
-            font-family: mugglenews;
-            font-size: 104px;
-        }
-        .p{
-            color:#d3a825;
-        }
-        .notice{
-        	width: 200px;
-        }
-        a{font-family: }
-    </style>
-</head>
-<body>
-    <div style="display: flex;">
-        <h1>Daily <b style="color: #d3a825;">P</b>rophet</h1>
-    </div>
-    <div class="notice">
-	    <h2><?=$title[0];?></h2>
-	    <a><?=$paragraph[0];?></a>    	
-    </div>
-</body>
-</html>
