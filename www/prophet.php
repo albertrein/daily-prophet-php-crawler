dc2e49670bfa5c422c1891cba4454583401485c3
albertrein <guilhermealberto8@gmail.com>
2020-10-05 20:30:34 -0300

    Inserting HTML tags.

--- www/prophet.php
+++ www/prophet.php
@@ -4,31 +4,38 @@
     use Symfony\Component\DomCrawler\Crawler;
     use Symfony\Component\CssSelector\CssSelectorConverter;
 
+    array_map("sitesIterate", $arrayOfSelectors);
 
-    function siteIterate($array){
-        echo "Site:".$array[0]."<br>";
+    function sitesIterate($array){
+        //echo "Site:".$array[0]."<br>";
         $html = file_get_contents(str_replace("\n", '', $array[0]));
         $crawler = new Crawler($html);
 
         for($i = 1; $i < count($array); $i++){
             if(strlen($array[$i]) > 2){
-                
+                if(substr($array[$i], 0, 1) == "h"){
+                    echo "<div>";
+                    $tagOfElement = substr($array[$i], 0, 2);
+                    $elementSelector = substr($array[$i], 2);
+                }else{
+                    $tagOfElement = substr($array[$i], 0, 1);
+                    $elementSelector = substr($array[$i], 1);
+                }
                 try{
-                    $texto = $crawler->filter($array[$i])->each(function (Crawler $node, $i) {
+                    $texto = $crawler->filter($elementSelector)->each(function (Crawler $node, $i) {
                         return $node->text();
                     });
-                    echo $texto[0]."<br>";
+                    echo "<".$tagOfElement.">".$texto[0]."</".$tagOfElement.">";
+                    if(strlen($array[$i + 1]) <= 2){
+                        echo "</div>";
+                    }
                 }catch(Exception $e){
                     die($e);
                 }
 
             }            
         }
-        echo "<br><br>";
     }
-
-
-    array_map("siteIterate", $arrayOfSelectors);
 
 
 ?>
