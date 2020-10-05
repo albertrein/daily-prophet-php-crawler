d0d85e75b8080d9a2f3fc178cf2cad3acf3beb16
albertrein <guilhermealberto8@gmail.com>
2020-10-04 22:47:25 -0300

    Added function to read and get Classes and IDs from website.

--- www/prophet.php
+++ www/prophet.php
@@ -1,19 +1,34 @@
 <?php
-//print_r($_COOKIE);
+    require('countries/autoload.php');
+    require('vendor/autoload.php');
+    use Symfony\Component\DomCrawler\Crawler;
+    use Symfony\Component\CssSelector\CssSelectorConverter;
 
-require('countries/autoload.php');
-require('vendor/autoload.php');
-use Symfony\Component\DomCrawler\Crawler;
-use Symfony\Component\CssSelector\CssSelectorConverter;
 
-$html = file_get_contents("https://oglobo.globo.com/");
-//$html = "<p>teste1gg</p>";
-$crawler = new Crawler($html);
-$title = $crawler->filter('.teaser__title.headline__title')->each(function (Crawler $node, $i) {
-    return $node->text();
-});
+    function siteIterate($array){
+        echo "Site:".$array[0]."<br>";
+        $html = file_get_contents(str_replace("\n", '', $array[0]));
+        $crawler = new Crawler($html);
 
-$paragraph = $crawler->filter('.teaser__subtitle.headline__subtitle')->each(function (Crawler $node, $i) {
-    return $node->text();
-});
+        for($i = 1; $i < count($array); $i++){
+            if(strlen($array[$i]) > 2){
+                
+                try{
+                    $texto = $crawler->filter($array[$i])->each(function (Crawler $node, $i) {
+                        return $node->text();
+                    });
+                    echo $texto[0]."<br>";
+                }catch(Exception $e){
+                    die($e);
+                }
+
+            }            
+        }
+        echo "<br><br>";
+    }
+
+
+    array_map("siteIterate", $arrayOfSelectors);
+
+
 ?>
