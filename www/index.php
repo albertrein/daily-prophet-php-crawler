bec2af3074a8652c89e0538a788f5c2920bdc1e5
albertrein <guilhermealberto8@gmail.com>
2020-10-12 11:09:34 -0300

    Adjusting capture os data.

--- www/index.php
+++ www/index.php
@@ -1,9 +1,1 @@
-<?php
-    if(!isset($_COOKIE['country'])){
-        require('view/set-local-news.html');
-        exit;
-    }
-    
-    require('view/daily-prophet.html');
-
-?>
+<?php require('view/daily-prophet.html'); ?>
